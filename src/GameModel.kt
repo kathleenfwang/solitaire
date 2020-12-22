923e1c735ab82e0afcf946082fd67b22d9c6dc1d
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-12-21 17:10:47 -0800

    finish game model

--- src/GameModel.kt
+++ src/GameModel.kt
@@ -30,7 +30,7 @@ class GameModel {
         if (wastePile.size > 0) {
             // check if we can use the last added card
             val card = wastePile.last()
-            if (playWasteCard(card)) wastePile.remove(card)
+            if (playCard(card)) wastePile.remove(card)
         }
     }
     fun onFoundationPileTap(ind:Int) {
@@ -38,25 +38,31 @@ class GameModel {
         if (foundationPile.cards.size > 0) {
             // check if we can use the last added card
             val card = foundationPile.cards.last()
-            if (playWasteCard(card,"foundation")) foundationPile.removeCard(card)
+            if (playCard(card)) foundationPile.removeCard(card)
         }
     }
-    private fun playWasteCard(card: Card, origin: String = "waste"): Boolean {
+    fun onTableauPileTap(tableauIndex: Int, cardIndex: Int) {
+        var tableauPile = tableauPiles[tableauIndex]
+        val cards = tableauPile.cards.subList(cardIndex,tableauPile.cards.lastIndex + 1)
+        // check if we can add our cards to a different tableau pile
+        // if we can, remove those cards from tableau pile
+        if (playCards(cards)) tableauPile.removeCards(cardIndex)
+    }
+
+    private fun playCards(cards: MutableList<Card>): Boolean {
+        // if cards is just one card, can call play card
+        if (cards.size == 1) return playCard(cards.first())
+        else {
+            tableauPiles.forEach { if (it.addCards(cards)) return true }
+        }
+        return false
+    }
+
+    private fun playCard(card: Card): Boolean {
         // 1. check if its playable in foundation piles
         // 2. check if its playable in tableau piles
-        if (origin == "foundation") {
-            tableauPiles.forEach {
-                if (it.addCards(mutableListOf(card))) return true
-            }
-        }
-        else {
-            foundationPiles.forEach {
-                if (it.addCard(card)) return true
-            }
-            tableauPiles.forEach {
-                if (it.addCards(mutableListOf(card))) return true
-            }
-        }
+            foundationPiles.forEach { if (it.addCard(card)) return true }
+            tableauPiles.forEach { if (it.addCards(mutableListOf(card))) return true }
         return false
     }
 }
