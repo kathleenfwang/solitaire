9694eb95005c684089a62ae0edd41a87e06dc66a
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-01-19 18:28:34 -0800

    add cards map to map cards value to name

--- src/Card.kt
+++ src/Card.kt
@@ -6,9 +6,10 @@ const val heart = "Heart"
 
 val reds = arrayOf(diamond,heart)
 val blacks = arrayOf(club,spade)
+val cardsMap = mapOf(0 to "Ace", 1 to "2", 2 to "3", 3 to "4", 4 to "5", 5 to "6", 6 to "7", 7 to "8", 8 to "9", 9 to "10", 10 to "Jack", 11 to "Queen", 12 to "King")
 data class Card(val value:Int, val suit: String, var faceUp:Boolean = false) {
     override fun toString(): String {
-        return "$value $suit $faceUp"
+        return "${cardsMap.getValue(value)} $suit $faceUp"
     }
 }
 
